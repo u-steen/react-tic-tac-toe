79d00d2e6912fed6d77354869695c2cbfd442c4c
Iustin <iustin.esanu7@gmail.com>
2023-08-01 16:55:30 +0300

    working version

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -3,27 +3,34 @@ import Square from "../square/square"
 import { useState, Fragment} from 'react'
 
 const Board = () => {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setisXNext] = useState(true);
 
-    const handleClick = (i) => {
-        if(squares[i])
-            return
-
-        const squaresCopy = squares.slice();
-
-        if(isXNext){
-            squaresCopy[i] = 'X';
+    function handleClick(i) {
+        if (calculateWinner(squares) || squares[i]) {
+          return;
         }
-        else{
-            squaresCopy[i] = '0';
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
         }
-
-        setSquares(squaresCopy);
-        setisXNext(!isXNext);
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
+
+    const winner = calculateWinner(squares);
+    let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
 return (
         <Fragment> 
+            <h1> {status} </h1>
             <div className="board-row">
                 <Square value={squares[0]} handleClickFn={ () => {handleClick(0)}}/>
                 <Square value={squares[1]} handleClickFn={ () => {handleClick(1)}}/>
@@ -42,6 +49,26 @@ return (
             </div>
         </Fragment>
 ) 
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board
